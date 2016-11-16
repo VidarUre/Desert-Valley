397a50340367ba2ab668e6716df97b8a2551cd8d
Kvalevaag5 <kvalevaag5@gmail.com>
2016-11-16 13:57:47 +0100

    Added UFO that follows camera

--- src/UFO.js
+++ src/UFO.js
@@ -6,11 +6,12 @@ class UFO {
 
     createUFO(camera, loader) {
         let UFOFile = "../Oblig4/models/space-invader.json";
-        this.addObject(camera, loader,UFOFile, 0, -100, -250, 50, 50, 50);
-        let node = THREE
+        this.loadObject(camera, loader,UFOFile, 0, -100, -250, 50, 50, 50);
+
+
     }
 
-    addObject(scene, loader, file, posx, posy, posz, scalex, scaley, scalez) {
+    loadObject(scene, loader, file, posx, posy, posz, scalex, scaley, scalez) {
 
         loader.load(file, function (object) {
 
@@ -22,7 +23,14 @@ class UFO {
             object.scale.x = scalex;
             object.scale.y = scaley;
             object.scale.z = scalez;
+            let rotation = [0.0, 0.10, 0.0];
+            object.rotation.x += rotation[0];
+            object.rotation.y += rotation[1];
+            object.rotation.z += rotation[2];
             scene.add(object);
+
         });
+
     }
+
 }
